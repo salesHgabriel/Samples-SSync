4be52bc66d7d3489c00bc2cb752c96076b8abbf8
Gabriel Sales <ghe_sales@hotmail.com>
2025-02-22 17:20:16 -0300

    Add mud blazor in poc front

--- PocServerFrontSync/Program.cs
+++ PocServerFrontSync/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using PocServerFrontSync;
+using PocServerFrontSync.Setup;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://api.backend") });
+builder.Services.AddMudServices();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(EndPoint.BaseURL) });
 
 await builder.Build().RunAsync();
